81bf814241bede996690526729663e6344fbf7bb
Kevin Häusler <kevin.haeusler@khdev.ch>
2024-04-26 10:11:48 +0000

    whatever

---
+++ src/main/java/ch/hslu/oop/sw03/exercise1/Demo.java
@@ -0,0 +1,2 @@
+package ch.hslu.oop.sw03.exercise1;public class Demo {
+}
---
+++ src/main/java/ch/hslu/oop/sw03/exercise1/MathHelper.java
@@ -0,0 +1,11 @@
+package ch.hslu.oop.sw03;
+
+public class Control {
+
+    public int max(int a, int b){
+        if a > b:{
+            return a;
+        }
+        return b;
+    }
+}
