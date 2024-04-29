f43169a465c7382f36196d452baf8ce1dec7ddc0
Kevin Häusler <kevin.haeusler@khdev.ch>
2024-04-29 12:21:12 +0000

    changing mercury tostring and testing aggregratestate in Demo class

--- src/main/java/ch/hslu/oop/sw05/exercise2/Demo.java
+++ src/main/java/ch/hslu/oop/sw05/exercise2/Demo.java
@@ -10,6 +10,14 @@ public class Demo {
         System.out.println(mercury);
 
         Temperature temperature = new Temperature(5f);
-        System.out.println(mercury.getAggregateState(temperature.getTemperatureInKelvin()));
+        System.out.println("Temperature: " + temperature.getTemperatureInCelsius() + "State: "
+                + mercury.getAggregateState(temperature.getTemperatureInKelvin()));
+        temperature.setTemperatureInKelvin(300f);
+        System.out.println("Temperature: " + temperature.getTemperatureInCelsius() + "State: "
+                + mercury.getAggregateState(temperature.getTemperatureInKelvin()));
+        temperature.changeTemperatureRelativeInKelvin(400f);
+        System.out.println("Temperature: " + temperature.getTemperatureInCelsius() + "State: "
+                + mercury.getAggregateState(temperature.getTemperatureInKelvin()));
+
     }
 }
--- src/main/java/ch/hslu/oop/sw05/exercise2/Mercury.java
+++ src/main/java/ch/hslu/oop/sw05/exercise2/Mercury.java
@@ -5,4 +5,9 @@ public class Mercury extends Element {
         super("Mercury", "Hg", 80, 234.3210f, 629.88f);
     }
 
+    @Override
+    public String toString() {
+        String description = super.toString();
+        return description + "\n| Warning: Toxic";
+    }
 }
